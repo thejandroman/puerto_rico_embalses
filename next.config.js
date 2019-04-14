bd6af89038dc29e766477b5eb55026143e2d8a48
Alejandro Figueroa <alejandrofigueroa@github.com>
2019-04-13 20:58:11 -0400

    Remove unused block from next config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
-//   webpack: function (config) {
-//     config.module.rules.push({
-//       test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
-//       use: {
-//         loader: 'url-loader',
-//         options: {
-//           limit: 100000,
-//           name: '[name].[ext]'
-//         }
-//       }
-//     })
-//     return config
-  //   }
   target: 'serverless'
 })
