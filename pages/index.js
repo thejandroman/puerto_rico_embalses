eb7af6f137c0b21358a298457221b572cc2eaaec
Alejandro Figueroa <alejandrofigueroa@github.com>
2020-09-22 11:53:48 -0400

    Use full url

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ Index.getInitialProps = async function () {
   const myEmbalses = new EmbalsesApi()
   const sites = myEmbalses.ids.join(',')
 
-  const res = await fetch(`//waterservices.usgs.gov/nwis/iv/?format=json&sites=${sites}&parameterCd=62616&siteStatus=all`, {mode: 'cors'})
+  const res = await fetch(`https://waterservices.usgs.gov/nwis/iv/?format=json&sites=${sites}&parameterCd=62616&siteStatus=all`, {mode: 'cors'})
   const data = await res.json()
 
   return {
